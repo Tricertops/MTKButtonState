ae14d7d387845b080dbe9d1b484be5df0809090f
Martin Kiss <martin.kiss@me.com>
2013-04-19 11:04:01 +0200

    Add usage example

--- Example/MTKAppDelegate.m
+++ Example/MTKAppDelegate.m
@@ -20,9 +20,32 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+    
+    UIViewController *viewController = [[UIViewController alloc] init];
+    viewController.view.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = viewController;
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:[UIFont buttonFontSize]];
+    button.normalState.title = @"Normal";
+    button.normalState.titleColor = [UIColor blackColor];
+    button.highlightedState.title = @"Highlighted";
+    button.highlightedState.titleColor = [UIColor redColor];
+    button.selectedState.title = @"Selected";
+    button.selectedState.titleColor = [UIColor blueColor];
+    button.selectedHighlightedState.title = @"Selected Highlighted";
+    button.selectedHighlightedState.titleColor = [UIColor greenColor];
+    [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    
+    button.center = CGPointMake(viewController.view.bounds.size.width/2, viewController.view.bounds.size.height/2);
+    [viewController.view addSubview:button];
+    
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)buttonTapped:(UIButton *)button {
+    button.selected = ! button.selected;
 }
 
 
