06df89d0fbd9c958cfb198a3a8afc21d5bd8e59e
Martin Kiss <martin.kiss@me.com>
2013-04-19 11:02:43 +0200

    Implement MTKButtonState class with proxy properties

--- MTKButtonState/MTKButtonState.h
+++ MTKButtonState/MTKButtonState.h
@@ -16,4 +16,17 @@
 
 @interface MTKButtonState : NSObject
 
+
+- (id)initWithButton:(UIButton *)button controlState:(UIControlState)controlState;
+@property (nonatomic, readonly, weak) UIButton *button;
+@property (nonatomic, readonly) UIControlState controlState;
+
+@property (nonatomic, readwrite) NSString *title;
+@property (nonatomic, readwrite) NSAttributedString *attributedTitle;
+@property (nonatomic, readwrite) UIColor *titleColor;
+@property (nonatomic, readwrite) UIColor *titleShadowColor;
+@property (nonatomic, readwrite) UIImage *image;
+@property (nonatomic, readwrite) UIImage *backgroundImage;
+
+
 @end
--- MTKButtonState/MTKButtonState.m
+++ MTKButtonState/MTKButtonState.m
@@ -12,6 +12,9 @@
 
 @interface MTKButtonState ()
 
+@property (nonatomic, readwrite, weak) UIButton *button;
+@property (nonatomic, readwrite) UIControlState controlState;
+
 @end
 
 
@@ -19,5 +22,148 @@
 
 
 @implementation MTKButtonState
+
+
+
+
+
+- (id)init {
+    return [self initWithButton:nil controlState:UIControlStateNormal];
+}
+
+
+- (id)initWithButton:(UIButton *)button controlState:(UIControlState)controlState {
+    self = [super init];
+    if (self) {
+        NSParameterAssert(button);
+        
+        self.button = button;
+        self.controlState = controlState;
+    }
+    return self;
+}
+
+
+
+#pragma mark Title
+
+
+- (NSString *)title {
+    return [self.button titleForState:self.controlState];
+}
+
+
+- (void)setTitle:(NSString *)title {
+    [self.button setTitle:title forState:self.controlState];
+}
+
+
++ (NSSet *)keyPathsForValuesAffectingTitle {
+    MTKButtonState *state; // because of @keypath
+    return [NSSet setWithObjects:@keypath(state, button), @keypath(state, controlState), @keypath(state, button.state), nil];
+}
+
+
+
+#pragma mark Attributed Title
+
+
+- (NSAttributedString *)attributedTitle {
+    return [self.button attributedTitleForState:self.controlState];
+}
+
+- (void)setAttributedTitle:(NSAttributedString *)attributedTitle {
+    [self.button setAttributedTitle:attributedTitle forState:self.controlState];
+}
+
+
++ (NSSet *)keyPathsForValuesAffectingAttributedTitle {
+    MTKButtonState *state; // because of @keypath
+    return [NSSet setWithObjects:@keypath(state, button), @keypath(state, controlState), @keypath(state, button.state), nil];
+}
+
+
+
+#pragma mark Title Color
+
+
+- (UIColor *)titleColor {
+    return [self.button titleColorForState:self.controlState];
+}
+
+
+- (void)setTitleColor:(UIColor *)titleColor {
+    [self.button setTitleColor:titleColor forState:self.controlState];
+}
+
+
++ (NSSet *)keyPathsForValuesAffectingTitleColor {
+    MTKButtonState *state; // because of @keypath
+    return [NSSet setWithObjects:@keypath(state, button), @keypath(state, controlState), @keypath(state, button.state), nil];
+}
+
+
+
+#pragma mark Title Shadow Color
+
+
+- (UIColor *)titleShadowColor {
+    return [self.button titleShadowColorForState:self.controlState];
+}
+
+
+- (void)setTitleShadowColor:(UIColor *)titleShadowColor {
+    [self.button setTitleShadowColor:titleShadowColor forState:self.controlState];
+}
+
+
++ (NSSet *)keyPathsForValuesAffectingTitleShadowColor {
+    MTKButtonState *state; // because of @keypath
+    return [NSSet setWithObjects:@keypath(state, button), @keypath(state, controlState), @keypath(state, button.state), nil];
+}
+
+
+
+#pragma mark Image
+
+
+- (UIImage *)image {
+    return [self.button imageForState:self.controlState];
+}
+
+
+- (void)setImage:(UIImage *)image {
+    [self.button setImage:image forState:self.controlState];
+}
+
+
++ (NSSet *)keyPathsForValuesAffectingImage {
+    MTKButtonState *state; // because of @keypath
+    return [NSSet setWithObjects:@keypath(state, button), @keypath(state, controlState), @keypath(state, button.state), nil];
+}
+
+
+
+#pragma mark Background Image
+
+
+- (UIImage *)backgroundImage {
+    return [self.button backgroundImageForState:self.controlState];
+}
+
+
+- (void)setBackgroundImage:(UIImage *)backgroundImage {
+    [self.button setBackgroundImage:backgroundImage forState:self.controlState];
+}
+
+
++ (NSSet *)keyPathsForValuesAffectingBackgroundImage {
+    MTKButtonState *state; // because of @keypath
+    return [NSSet setWithObjects:@keypath(state, button), @keypath(state, controlState), @keypath(state, button.state), nil];
+}
+
+
+
+
 
 @end
